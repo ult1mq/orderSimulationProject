26dfa8be556b7b8f7cdd3676d36136a80caf8ecc
Sidorenko <sidorenko3025@gmail.com>
2025-06-20 18:04:35 +0300

    logger -> WebClientConfig.kt

--- order-service/src/main/kotlin/org/ult1mma/orderservice/config/WebClientConfig.kt
+++ order-service/src/main/kotlin/org/ult1mma/orderservice/config/WebClientConfig.kt
@@ -1,12 +1,18 @@
 package org.ult1mma.orderservice.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class WebClientConfig {
-    @Bean
-    fun webClient(): WebClient = WebClient.builder().build()
 
+    private val logger = LoggerFactory.getLogger(WebClientConfig::class.java)
+
+    @Bean
+    fun webClient(): WebClient {
+        logger.info("Создаётся бин WebClient (reactive HTTP клиент)")
+        return WebClient.builder().build()
+    }
 }
