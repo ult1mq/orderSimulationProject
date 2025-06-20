adf569cfca0245588de176a4c04388d229d8d557
Sidorenko <sidorenko3025@gmail.com>
2025-06-20 18:09:55 +0300

    logger -> UserService.kt

--- user-service/src/main/kotlin/org/ult1mma/userservice/service/UserService.kt
+++ user-service/src/main/kotlin/org/ult1mma/userservice/service/UserService.kt
@@ -1,15 +1,43 @@
 package org.ult1mma.userservice.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.ult1mma.userservice.model.User
 import org.ult1mma.userservice.repository.UserRepository
-import java.util.Optional
 
 @Service
 class UserService(private val userRepository: UserRepository) {
-    fun getAll(): List<User> = userRepository.findAll()
-    fun getById(id: Long): User? = userRepository.findById(id).orElse(null)
-    fun create(user: User): User = userRepository.save(user)
-    fun delete(id: Long) = userRepository.deleteById(id)
 
+    private val logger = LoggerFactory.getLogger(UserService::class.java)
+
+    fun getAll(): List<User> {
+        logger.info("Получение списка всех пользователей")
+        val users = userRepository.findAll()
+        logger.info("Найдено пользователей: {}", users.size)
+        return users
+    }
+
+    fun getById(id: Long): User? {
+        logger.info("Запрос пользователя по id={}", id)
+        val user = userRepository.findById(id).orElse(null)
+        if (user == null) {
+            logger.warn("Пользователь с id={} не найден", id)
+        } else {
+            logger.info("Пользователь с id={} найден: {}", id, user)
+        }
+        return user
+    }
+
+    fun create(user: User): User {
+        logger.info("Создание пользователя: {}", user)
+        val saved = userRepository.save(user)
+        logger.info("Пользователь успешно создан: id={}", saved.id)
+        return saved
+    }
+
+    fun delete(id: Long) {
+        logger.info("Удаление пользователя с id={}", id)
+        userRepository.deleteById(id)
+        logger.info("Пользователь с id={} удалён", id)
+    }
 }
