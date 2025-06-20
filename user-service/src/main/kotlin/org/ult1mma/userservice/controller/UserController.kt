52b33654d55f732eb19f54961085775a987cd25c
Sidorenko <sidorenko3025@gmail.com>
2025-06-20 18:09:00 +0300

    logger -> UserController.kt

--- user-service/src/main/kotlin/org/ult1mma/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/org/ult1mma/userservice/controller/UserController.kt
@@ -1,5 +1,6 @@
 package org.ult1mma.userservice.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,21 +14,41 @@ import org.ult1mma.userservice.service.UserService
 
 @RestController
 @RequestMapping("/users")
-class UserController (private val userService: UserService) {
+class UserController(private val userService: UserService) {
+
+    private val logger = LoggerFactory.getLogger(UserController::class.java)
 
     @GetMapping
-    fun getAll() = userService.getAll()
+    fun getAll(): List<User> {
+        logger.info("Получен запрос: получить всех пользователей")
+        return userService.getAll()
+    }
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long) =
-        userService.getById(id)?.let { ResponseEntity.ok(it) }
-            ?: ResponseEntity.notFound().build()
+    fun getById(@PathVariable id: Long): ResponseEntity<User> {
+        logger.info("Получен запрос: получить пользователя по id={}", id)
+        val user = userService.getById(id)
+        return if (user != null) {
+            logger.info("Пользователь с id={} найден", id)
+            ResponseEntity.ok(user)
+        } else {
+            logger.warn("Пользователь с id={} не найден", id)
+            ResponseEntity.notFound().build()
+        }
+    }
 
-    @PostMapping()
-    fun create(@RequestBody user: User) = userService.create(user)
+    @PostMapping
+    fun create(@RequestBody user: User): User {
+        logger.info("Получен запрос: создать пользователя: {}", user)
+        val saved = userService.create(user)
+        logger.info("Пользователь создан: id={}", saved.id)
+        return saved
+    }
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long) = userService.delete(id)
-
-
+    fun delete(@PathVariable id: Long) {
+        logger.info("Получен запрос: удалить пользователя id={}", id)
+        userService.delete(id)
+        logger.info("Пользователь с id={} удалён", id)
+    }
 }
